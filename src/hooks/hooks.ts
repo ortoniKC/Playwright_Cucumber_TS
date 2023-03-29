2cfb48b8af00b5dc490fe82784195cdbfc1b3dea
ortoniKC <koushik350@gmail.com>
2023-03-29 12:34:38 +0530

    📦 NEW: Screenshots on failure

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,16 +1,37 @@
-import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After, BeforeStep, AfterStep, Status } from "@cucumber/cucumber";
+import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
 let browser: Browser;
+let context: BrowserContext;
+
+BeforeAll(async function () {
+    browser = await chromium.launch({ headless: false });
+});
 
 Before(async function () {
-    browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage();
+    context = await browser.newContext();
+    const page = await context.newPage();
     pageFixture.page = page;
 });
-After(async function () {
+
+AfterStep(async function ({ pickle, result }) {
+    const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" })
+    await this.attach(img, "image/png");
+});
+
+After(async function ({ pickle, result }) {
+    console.log(result?.status);
+    // screenshot
+    if (result?.status == Status.FAILED) {
+        const img = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" })
+        await this.attach(img, "image/png");
+    }
     await pageFixture.page.close();
+    await context.close();
+});
+
+AfterAll(async function () {
     await browser.close();
 })
 
