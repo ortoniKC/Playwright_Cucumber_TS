20424769bdc79b63a777932e645f973cb541f81e
ortoniKC <koushik350@gmail.com>
2023-04-01 16:33:49 +0530

    📦 NEW: Parallel & rerun

---
+++ src/helper/init.ts
@@ -0,0 +1,9 @@
+const fs = require("fs-extra");
+try {
+    fs.ensureDir("test-results");
+    fs.emptyDir("test-results")
+
+} catch (error) {
+    console.log("Folder not created! " + error);
+
+}
--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -6,7 +6,7 @@ let browser: Browser;
 let context: BrowserContext;
 
 BeforeAll(async function () {
-    browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: !false });
 });
 
 Before(async function () {
