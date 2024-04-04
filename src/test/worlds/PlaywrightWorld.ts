c05ab71e57d5de3db75c0f5f1e6ff489e674c804
Alex Sicoe <alexsicoe96@gmail.com>
2024-04-04 16:57:43 +0300

    fix world

--- src/test/worlds/PlaywrightWorld.ts
+++ src/test/worlds/PlaywrightWorld.ts
@@ -1,10 +1,18 @@
-import { World } from '@cucumber/cucumber';
+import { IWorldOptions, setWorldConstructor, World } from '@cucumber/cucumber';
 import { IFixture } from '../../hooks/FixtureManager';
 
-export default class PlaywrightWorld extends World {
-    fixture: IFixture;
-
-    constructor(options: any) {
-      super(options);
-    }
+export interface IPlaywrightWorld extends World {
+  fixture: IFixture;
+  store: any;
 }
+
+export default class PlaywrightWorld extends World implements IPlaywrightWorld {
+  fixture: IFixture;
+  store: any;
+
+  constructor(options: IWorldOptions) {
+    super(options);
+  }
+}
+
+setWorldConstructor(PlaywrightWorld);
