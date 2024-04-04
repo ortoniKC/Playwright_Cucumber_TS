bf778282788128d55124003a7b31dbdab906b3cf
Alex Sicoe <alexsicoe96@gmail.com>
2024-04-04 14:42:04 +0300

    add debug profile to cucumber config

--- config/cucumber.js
+++ config/cucumber.js
@@ -1,49 +1,44 @@
+const tags = process.env.npm_config_tags || "";
+
+const commonConfig = {
+    formatOptions: {
+        snippetInterface: "async-await"
+    },
+    paths: [
+        "src/test/features/**/*"
+    ],
+    publishQuiet: true,
+    dryRun: false,
+    require: [
+        "src/test/steps/**/*.ts",
+        "src/hooks/hooks.ts"
+    ],
+    requireModule: [
+        "ts-node/register"
+    ],
+    format: [
+        "progress-bar",
+        "html:test-results/cucumber-report.html",
+        "json:test-results/cucumber-report.json",
+        "rerun:@rerun.txt"
+    ],
+};
+
+const { paths, ...commonConfigWithoutPaths } = commonConfig;
+
 module.exports = {
     default: {
-        tags: process.env.npm_config_tags || "",
-        formatOptions: {
-            snippetInterface: "async-await"
-        },
-        paths: [
-            "src/test/features/**/*"
-        ],
-        publishQuiet: true,
-        dryRun: false,
-        // retry: 2,
-        require: [
-            "src/test/steps/**/*.ts",
-            "src/hooks/hooks.ts"
-        ],
-        requireModule: [
-            "ts-node/register"
-        ],
-        format: [
-            "progress-bar",
-            "html:test-results/cucumber-report.html",
-            "json:test-results/cucumber-report.json",
-            "rerun:@rerun.txt"
-        ],
-        parallel: 8
+        ...commonConfig,
+        tags: tags,
+        parallel: 12,
+    },
+    debug: {
+        ...commonConfig,
+        tags: `${tags ? `@debug and ${tags}` : '@debug'}`,
+        parallel: 1,
     },
     rerun: {
-        formatOptions: {
-            snippetInterface: "async-await"
-        },
-        publishQuiet: true,
-        dryRun: false,
-        require: [
-            "src/test/steps/*.ts",
-            "src/hooks/hooks.ts"
-        ],
-        requireModule: [
-            "ts-node/register"
-        ],
-        format: [
-            "progress-bar",
-            "html:test-results/cucumber-report.html",
-            "json:test-results/cucumber-report.json",
-            "rerun:@rerun.txt"
-        ],
-        parallel: 2
+        ...commonConfigWithoutPaths,
+        parallel: 1
     }
 }
